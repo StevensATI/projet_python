f961580b218fee9023d97e91077699bc7bd135b9
StevensATI <stevens.lecurieux@etud.univ-paris8.fr>
2025-12-09 15:11:34 +0100

    copie des fichiers de nathan dans mon dossier

---
+++ _stevens/Building Generator V1/scripts/balcon_generator.py
@@ -0,0 +1,63 @@
+##########
+
+import maya.cmds as cmds
+import random
+import sys
+import os
+
+##########
+
+def balcon(unit, unit_x, unit_y, unit_z, largeur, hauteur, profondeur):
+    
+    cmds.group(empty=True, n="balcon_grp")
+    
+    for y in range(unit_y):
+        
+        for x in range(unit_x):
+            
+            prefab = cmds.instance("tmp_balcon_grille")[0]
+            prefab = cmds.rename(prefab, f"balcon_sud_{y}_{x}")
+        
+            cmds.move((-largeur/2)+(unit/2)+(unit*x), (unit*y), (profondeur/2), prefab, r=True)
+            
+            cmds.parent(prefab, "balcon_grp")
+    
+    for y in range(unit_y):
+        
+        for z in range(unit_z):
+            
+            prefab = cmds.instance("tmp_balcon_grille")[0]
+            prefab = cmds.rename(prefab, f"balcon_ouest_{y}_{z}")
+            
+            cmds.rotate(0, -90, 0, prefab, r=True)
+            cmds.move((-largeur/2), (unit*y), (-profondeur/2)+(unit/2)+(unit*z), prefab, r=True)
+            
+            cmds.parent(prefab, "balcon_grp")
+            
+    for y in range(unit_y):
+        
+        for x in range(unit_x):
+            
+            prefab = cmds.instance("tmp_balcon_grille")[0]
+            prefab = cmds.rename(prefab, f"balcon_nord_{y}_{x}")
+            
+            cmds.rotate(0, -180, 0, prefab, r=True)
+            cmds.move((largeur/2)+(-unit/2)+(-unit*x), (unit*y), (-profondeur/2), prefab, r=True)
+            
+            cmds.parent(prefab, "balcon_grp")
+            
+    for y in range(unit_y):
+        
+        for z in range(unit_z):
+            
+            prefab = cmds.instance("tmp_balcon_grille")[0]
+            prefab = cmds.rename(prefab, f"balcon_est_{y}_{z}")
+            
+            cmds.rotate(0, -270, 0, prefab, r=True)
+            cmds.move((largeur/2), (unit*y), (profondeur/2)+(-unit/2)+(-unit*z), prefab, r=True)
+            
+            cmds.parent(prefab, "balcon_grp")
+            
+    cmds.parent("balcon_grp", "building_generator_grp")
+            
+##########
